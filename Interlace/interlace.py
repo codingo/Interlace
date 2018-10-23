f119f2d6ee03b507f879131a53c99b447ed85cb9
Michael Skelton <michael@codingo.com.au>
2018-10-23 20:41:48 +1000

    Output Strucutre

--- Interlace/interlace.py
+++ Interlace/interlace.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 import sys
-from lib.core.input import InputParser
+from lib.core.input import InputParser, InputHelper
+from lib.core.output import OutputHelper, Level
 
 def main():
     parser = InputParser()
     arguments = parser.parse(sys.argv[1:])
+    output = OutputHelper(arguments)
 
+    output.print_banner()
+
+    for target in InputHelper.process_targets(arguments):
+        for command in InputHelper.process_commands(arguments):
+            output.terminal(Level.THREAD, target, command)
 
 if __name__ == "__main__":
     main()
