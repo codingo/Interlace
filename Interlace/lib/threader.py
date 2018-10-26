4ab475dd9823ca1cfe9e195320f7aad7538080f8
Charelle Collett <charcol@blackhats.net.au>
2018-10-26 11:07:03 +1000

    modified threader to be more cpu efficient

--- Interlace/lib/threader.py
+++ Interlace/lib/threader.py
@@ -3,12 +3,20 @@ import os
 
 
 class Worker(object):
-    def __init__(self, pool):
-        self.pool = pool
+    def __init__(self, queue, timeout, output):
+        self.queue = queue
+        self.timeout = timeout
+        self.output = output
 
-    def __call__(self, task, output, timeout):
-        self.run_task(task)
-        self.pool.workers.append(self)
+    def __call__(self):
+        while True:
+            try:
+                # get task from queue
+                task = self.queue.pop(0)
+                # run task
+                self.run_task(task)
+            except IndexError:
+                break
 
     @staticmethod
     def run_task(task):
@@ -17,28 +25,47 @@ class Worker(object):
 
 class Pool(object):
     def __init__(self, max_workers, queue, timeout, output):
+
+        # check if there are enough workers
+        if max_workers <= 0:
+            raise ValueError("Workers must be >= 1")
+
+        # check if the queue is empty
+        if not queue:
+            raise ValueError("The queue is empty")
+
         self.queue = queue
-        self.workers = [Worker(self) for w in range(max_workers)]
         self.timeout = timeout
         self.output = output
+        self.max_workers = max_workers
 
     def run(self):
-        while True:
 
-            # make sure resources are available
-            if not self.workers:
-                continue
+        workers = [Worker(self.queue, self.timeout, self.output) for w in range(self.max_workers)]
+        threads = []
 
-            # check if the queue is empty
-            if not self.queue:
-                break
 
-            # get a worker
-            worker = self.workers.pop(0)
-
-            # get task from queue
-            task = self.queue.pop(0)
-
-            # run
-            thread = threading.Thread(target=worker, args=(task, self.output, self.timeout))
+        # run
+        for worker in workers:
+            thread = threading.Thread(target=worker)
             thread.start()
+            threads.append(thread)
+
+        # wait until all workers have completed their tasks
+        for thread in threads:
+            thread.join()
+
+if __name__ == "__main__":
+    tasks = ["sleep 1",
+             "sleep 2",
+             "sleep 3",
+             "sleep 4",
+             "sleep 5",
+             "sleep 6",
+             "sleep 7",
+             "sleep 8",
+             "sleep 9",
+             "sleep 1",
+             "echo 'Char!'"]
+    p = Pool(4, tasks, 0, 0)
+    p.run()
