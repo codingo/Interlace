97f4e25ae8827fea45244c866361786d393b8217
Michael Skelton <886344+codingo@users.noreply.github.com>
2019-01-06 12:27:02 +1000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,41 @@
+""" __Doc__ File handle class """
+from setuptools import find_packages, setup
+from Interlace.lib.core.__version__ import __version__
+
+
+def dependencies(imported_file):
+    """ __Doc__ Handles dependencies """
+    with open(imported_file) as file:
+        return file.read().splitlines()
+
+
+with open("README.md") as file:
+    num_installed = False
+    try:
+        import numpy
+        num_installed = True
+    except ImportError:
+        pass
+    setup(
+        name="Interlace",
+        license="GPLv3",
+        description="Adds Multi Threading and CIDR support for applications that don't support it, "
+                    "or better managed threads for those that do."
+                    "Multiple commands over multiple hosts.",
+        long_description=file.read(),
+        author="codingo",
+        version=__version__,
+        author_email="codingo@protonmail.com",
+        url="https://github.com/codingo/Interlace",
+        packages=find_packages(exclude=('tests')),
+        package_data={'VHostScan': ['*.txt']},
+        entry_points={
+            'console_scripts': [
+                'Interlace = Interlace.Interlace:main'
+            ]
+        },
+        install_requires=dependencies('requirements.txt'),
+        setup_requires=['pytest-runner',
+                        '' if num_installed else 'numpy==1.12.0'],
+        tests_require=dependencies('test-requirements.txt'),
+        include_package_data=True)
