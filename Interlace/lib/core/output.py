927d526927cce9ceaf16e584f839e465199a7a1d
prodigysml <sajeeb.lohani@bugcrowd.com>
2022-09-05 22:41:10 +1000

    attempt at colours without colorclass

--- Interlace/lib/core/output.py
+++ Interlace/lib/core/output.py
@@ -7,6 +7,19 @@ from colorclass import disable_all_colors
 from Interlace.lib.core.__version__ import __version__
 
 
+class bcolors:
+    # credit to: https://stackoverflow.com/questions/287871/how-do-i-print-colored-text-to-the-terminal
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
 class OutputHelper(object):
     def __init__(self, arguments):
         if arguments.nocolor:
@@ -30,8 +43,8 @@ class OutputHelper(object):
             return
 
         formatting = {
-            0: Color('{autoblue}[VERBOSE]{/autoblue}'),
-            1: Color('{autogreen}[THREAD]{/autogreen}'),
+            0: f'{bcolors.OKBLUE}[VERBOSE]{bcolors.ENDC}',
+            1: f'{bcolors.OKGREEN}[THREAD]{bcolors.ENDC}',
             3: Color('{autobgyellow}{autored}[ERROR]{/autored}{/autobgyellow}')
         }
 
